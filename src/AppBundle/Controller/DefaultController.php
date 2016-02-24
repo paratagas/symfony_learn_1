f5eb71a13a53878e22240874b6b6775b54553211
Yauheni Svirydzenka <partagas@mail.ru>
2016-02-24 18:06:46 +0300

    Learned Symfony book Security chapter

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
@@ -17,5 +19,25 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+
+    /**
+     * @Route("/admin", name="_admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+
+    /**
+     * @Route("/role_user", name="_role_user")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function roleUserAction()
+    {
+        // можно прописать права доступа так:
+        // $this->denyAccessUnlessGranted('ROLE_USER', null, 'Что-то не заходится внутрь!');
+        // но лучше сделать это в аннотации, подключив Security
+        return new Response('<html><body>Role User page!</body></html>');
     }
 }
--- src/AppBundle/Controller/test.php
+++ src/AppBundle/Controller/test.php
@@ -12,3 +12,28 @@ function anotherFunc($param_1)
     $param_1 .= " Now added to parameter 1";
     return $param_1;
 }
+
+// php 7
+// Integers
+echo 1 <=> 1; // 0
+echo PHP_EOL;
+echo 1 <=> 2; // -1
+echo PHP_EOL;
+echo 2 <=> 1; // 1
+echo PHP_EOL;
+
+// Floats
+echo 1.5 <=> 1.5; // 0
+echo PHP_EOL;
+echo 1.5 <=> 2.5; // -1
+echo PHP_EOL;
+echo 2.5 <=> 1.5; // 1
+echo PHP_EOL;
+
+// Strings
+echo "a" <=> "a"; // 0
+echo PHP_EOL;
+echo "a" <=> "b"; // -1
+echo PHP_EOL;
+echo "b" <=> "a"; // 1
+echo PHP_EOL;
